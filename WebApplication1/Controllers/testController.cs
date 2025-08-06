222ceaf5116a668da22fadb1c0ac59d0e3e5124d
thanhtv <trvthanh410@gmail.com>
2025-08-06 20:01:19 +0700

    Initial commit

---
+++ WebApplication1/Controllers/testController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/test")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("SayHello")]
+        public string Get(string name)
+        {
+            return "Xin chao tk lol " + name;
+        }
+        [HttpPost("AddName")]
+        public string AddName(string name)
+        {
+            return "Add rồi: " + name;
+        }
+    }
+}
